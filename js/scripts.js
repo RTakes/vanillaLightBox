c388dfe80570ff3e626bef84c2be83ddcef15251
Rick Takes <rick@theparkmart.com>
2015-03-27 13:33:29 -0700

    Added infinite scroll and refactored to oop

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,43 @@
 /**************************
 * Initialize VanillaLb 
 **************************/
+var imagesLoadedCount = 0;
 
 var vanillaLb = new VanillaLb({
   resolution : 'low_resolution',
-  instagramTag : 'dog' 
+  instagramClientId : clientId,
+  instagramTag : 'dog',
+  imagesLoadedCallback : imagesLoaded
 });
 
+function imagesLoaded(){
+ imagesLoadedCount++;
+ if(imagesLoadedCount === vanillaLb.imageCount){
+  vanillaLb.loadingToggle('hide');
+ }
+};
+
+
+//Prevent rapid repeaded loading on scroll
+var throttled = throttle(retrieveData, 1000);
+window.onscroll = function(){
+  var pageHeight = document.documentElement.scrollHeight; //max scrollable height
+  var scrollPosition = document.documentElement.scrollTop || window.pageYOffset; //current scroll position
+  var remainingHeight = document.documentElement.clientHeight; //remaining scrollable height
+
+  if(pageHeight - (scrollPosition + remainingHeight) < vanillaLb.scrollHeight){
+    throttled();
+  }
+};
 
 /**************************
 * Get data from instagram
 **************************/
-
-//var clientId = 'YOUR CLIENT ID';
-
-function requestUrl(baseUrl, clientId, cbName){
-  return baseUrl+''+clientId+'&callback='+cbName;
-};
-
 function retrieveData(url){
+  vanillaLb.loadingToggle('show');
+  
   if(!url){
-    var baseUrl = vanillaLb.instagramUrl;
-    var url = requestUrl( baseUrl, clientId, 'vanillaLb.getData'); 
+    var url = vanillaLb.instagramNextUrl || vanillaLb.instagramUrl+'vanillaLb.getData';
   }
 
   var scriptTag = document.createElement('SCRIPT');
@@ -33,11 +49,6 @@ function retrieveData(url){
 
 retrieveData();
 
-//Get next set of photos
-// console.log(vanillaLb.currentData.pagination.next_url);
-// retrieveData(vanillaLb.currentData.pagination.next_url);
-
-
 
 /**************************
 * When dom is ready
@@ -45,7 +56,7 @@ retrieveData();
 document.addEventListener("DOMContentLoaded", function() {
   //Show lightbox when photo is clicked
   document.getElementsByClassName('img-grid')[0].addEventListener('click', function(e) {
-    vanillaLb.goToPhoto(e.target.dataset.index);
+    vanillaLb.goToPhoto(e.path[1].dataset.index);
     vanillaLb.openLightbox();
   });
 
@@ -64,6 +75,7 @@ document.addEventListener("DOMContentLoaded", function() {
     next[i].addEventListener('click', function(e) {
       e.preventDefault();
       vanillaLb.next();
+      retrieveData(vanillaLb.instagramNextUrl);      
     });
   }
 
@@ -76,25 +88,32 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
+//Throttle based on underscores throttle method
+function throttle(func, wait) {
+  var context, args, result;
+  var timeout = null;
+  var previous = 0;
 
-/**************************
-* Page setup functions
-**************************/
-function buildImgGrid(data){
-  //instagram resolution options: low_resolution, standard_resolution, thumbnail 
-  resolution = vanillaLb.resolution; 
-  var images = vanillaLb.currentData.data;
+  var later = function() {
+    previous = new Date;
+    timeout = null;
+    result = func.apply(context, args);
+  };
 
-  for(var i=0; i<images.length; i++){
-
-    var li = document.createElement('li');
-    li.className = 'photo-block';
-
-    var img = document.createElement('img');
-    img.src = images[i].images[resolution].url;
-    img.dataset.index = i;
-
-    li.appendChild(img);
-    document.getElementsByClassName('img-grid')[0].appendChild(li);
-  }
+  return function() {
+    var now = new Date;
+    if (!previous) previous = now;
+    var remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0) {
+      clearTimeout(timeout);
+      timeout = null;
+      previous = now;
+      result = func.apply(context, args);
+    } else if (!timeout) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
 };
--- js/vanilla-lightbox.js
+++ js/vanilla-lightbox.js
@@ -4,8 +4,28 @@ var VanillaLb = function(opt){
   this.currentData = {};
   this.previousData = [];
   this.imageCount = 0;
+  this.imagesLoadedCallback = opt.imagesLoadedCallback || null;
+  this.scrollHeight = opt.scrolllHeight || 100;
+  this.instagramClientId = opt.instagramClientId,
   this.instagramTag = opt.instagramTag || 'cat';
-  this.instagramUrl = 'https://api.instagram.com/v1/tags/'+this.instagramTag+'/media/recent?client_id=';
+  this.instagramUrl = 'https://api.instagram.com/v1/tags/'+this.instagramTag+'/media/recent?client_id='+this.instagramClientId+'&callback=';
+  this.instagramNextUrl = '';
+
+  function initializeDomElements(){
+    var ul = document.createElement('ul');
+    ul.classList.add('img-grid');
+
+    var lightbox = document.createElement('div');
+    lightbox.classList.add('lightbox');
+
+  } 
+
+  //initialize elements
+  //ul.img-grid
+  //div.lightbox.lb
+    //nav.lb-nav
+    //div.img-frame  
+  //div.lb-overlay.lb
 
 };
 
@@ -17,14 +37,28 @@ VanillaLb.prototype.getData = function(gallery){
     }else{
       //Save previous image set then load more images
       this.previousData.push(this.currentData);
-      this.currentData = gallery;
-      //this.currentData.data = this.currentData.data.concat(gallery.data);
+      //this.currentData = gallery;
+      this.currentData.data = this.currentData.data.concat(gallery.data);
     }
     
-    this.imageCount = this.currentData.data.length;
-    buildImgGrid(this.currentData.data);
+    this.instagramNextUrl = gallery.pagination.next_url;
+    this.imageCount = this.imageCount + gallery.data.length;
+    this.buildImgGrid(gallery.data);
   }else{
     console.error(data.meta.code+': '+ data.meta.error_message);
+  }
+};
+
+VanillaLb.prototype.loadingToggle = function(action){
+  action  = action || 'show';
+
+  var elements = document.getElementsByClassName('loading');
+  for(var i=0; i<elements.length; i++) {
+    if(action === 'show'){
+      elements[i].classList.add('active');
+    }else{
+     elements[i].classList.remove('active'); 
+    }
   }
 };
 
@@ -56,8 +90,10 @@ VanillaLb.prototype.goToPhoto = function(index){
   if(index>=0 && index<this.imageCount){
     this.currentImage = parseInt(index);
 
-    var title = document.createElement('h3');
-    title.innerHTML = this.currentData.data[this.currentImage].caption.text;
+    if(this.currentData.data[this.currentImage].caption){    
+      var title = document.createElement('h3');
+      title.innerHTML = this.currentData.data[this.currentImage].caption.text;
+    }
 
     var img = document.createElement('img');
     img.src = this.currentData.data[this.currentImage].images[this.resolution].url;
@@ -81,3 +117,34 @@ VanillaLb.prototype.next = function(){
 VanillaLb.prototype.prev = function(){
   this.goToPhoto(this.currentImage-1);
 };
+
+
+//Setup and append to the page
+VanillaLb.prototype.buildImgGrid = function(images){
+  for(var i=0; i<images.length; i++){
+
+    var li = document.createElement('li');
+    li.className = 'photo-block';
+    if(this.currentData.data.length > images.length){
+      li.dataset.index = i+this.currentData.data.length-images.length;
+    }else{
+      li.dataset.index = i;
+    }
+
+    var div = document.createElement('div');
+    div.className = 'img-overlay';
+
+    var img = document.createElement('img');
+    img.src = images[i].images[this.resolution].url;
+
+    if(this.imagesLoadedCallback){
+      img.onload = this.imagesLoadedCallback;
+    }
+     
+    li.appendChild(div);
+    li.appendChild(img);
+    document.getElementsByClassName('img-grid')[0].appendChild(li);
+  }
+};
+
+
